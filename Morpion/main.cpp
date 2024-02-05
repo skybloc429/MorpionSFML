110fc1fb7f9f4266110b31ff524d46258b374e30
skybloc429 <147525314+skybloc429@users.noreply.github.com>
2024-02-05 17:03:06 +0100

    sf

--- Morpion/main.cpp
+++ Morpion/main.cpp
@@ -22,67 +22,67 @@ bool turn(Render render, Morpion* morpion, int turnCounter) {
 }
 
 
-//int main(int argc, char** argv)
-//{
-//    //Création d'une fenêtre
-//    Render myRenderer{new sf::RenderWindow(sf::VideoMode(640, 480), "SFML"), new sf::Event, 640, 480};
-//    Morpion* myMorpion;
-//    myMorpion = new Morpion;
-//
-//    Player* player1 = nullptr;
-//    player1 = new Player{ "William", Symbol::X, 0};
-//
-//    Player* player2 = nullptr;
-//    player2 = new Player{ "Bot", Symbol::O, 0 };
-//
-//
-//    //GameLoop
-//    myMorpion->drawBoard(myRenderer);
-//    myMorpion->currentPlayer = player1;
-//    int turnCounter = 0;
-//    while (myRenderer.pWindow->isOpen())
-//    {
-//
-//        
-//
-//
-//        //EVENT
-//        int inputState = updateInput(myRenderer);
-//        //PRINT(myMorpion.currentPlayer->name)
-//        if (inputState == 0) {
-//            //PRINT("Pas d'Input")
-//            continue;
-//        }
-//        else if(inputState == 1) {
-//            if (!turn(myRenderer, myMorpion, turnCounter))
-//            {
-//                continue;
-//            };
-//        }
-//   
-//
-//
-//
-//        //PRINT("Action")
-//
-//        //UPDATE
-//
-//        //DRAW
-//        if (myMorpion->checkEnd(Symbol::X) || myMorpion->checkEnd(Symbol::O)) {
-//            PRINT("Partie terminé, joueur gagnant :")
-//            PRINT(myMorpion->currentPlayer->name);
-//            //break;
-//        }
-//
-//        // Transition next it
-//        myMorpion->currentPlayer = (turnCounter % 2 == 0) ? player2 : player1;
-//        turnCounter++;
-//
-//
-//    }
-//
-//    return 0;
-//}
+int main(int argc, char** argv)
+{
+    //Création d'une fenêtre
+    Render myRenderer{new sf::RenderWindow(sf::VideoMode(640, 480), "SFML"), new sf::Event, 640, 480};
+    Morpion* myMorpion;
+    myMorpion = new Morpion;
+
+    Player* player1 = nullptr;
+    player1 = new Player{ "William", Symbol::X, 0};
+
+    Player* player2 = nullptr;
+    player2 = new Player{ "Bot", Symbol::O, 0 };
+
+
+    //GameLoop
+    myMorpion->drawBoard(myRenderer);
+    myMorpion->currentPlayer = player1;
+    int turnCounter = 0;
+    while (myRenderer.pWindow->isOpen())
+    {
+
+        
+
+
+        //EVENT
+        int inputState = updateInput(myRenderer);
+        //PRINT(myMorpion.currentPlayer->name)
+        if (inputState == 0) {
+            //PRINT("Pas d'Input")
+            continue;
+        }
+        else if(inputState == 1) {
+            if (!turn(myRenderer, myMorpion, turnCounter))
+            {
+                continue;
+            };
+        }
+   
+
+
+
+        //PRINT("Action")
+
+        //UPDATE
+
+        //DRAW
+        if (myMorpion->checkEnd(Symbol::X) || myMorpion->checkEnd(Symbol::O)) {
+            PRINT("Partie terminé, joueur gagnant :")
+            PRINT(myMorpion->currentPlayer->name);
+            //break;
+        }
+
+        // Transition next it
+        myMorpion->currentPlayer = (turnCounter % 2 == 0) ? player2 : player1;
+        turnCounter++;
+
+
+    }
+
+    return 0;
+}
 
 #include <iostream>
 #include <Ws2tcpip.h>  // Ajout de l'en-tête pour inet_pton
@@ -157,7 +157,7 @@ void CloseConnection(ConnectionData& connectionData) {
     WSACleanup();
 }
 
-int main() {
+/*int main() {
     ConnectionData connectionData;
     connectionData.getRequest = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
 
@@ -172,4 +172,4 @@ int main() {
     }
 
     return 0;
-}
+}*/
